5fc3683724c263c837025e2e3b6674f610702704
namidapoo <83203852+ve1997@users.noreply.github.com>
2024-12-29 08:00:13 +0900

    feat: ヘッダー追加

---
+++ app/recap/components/header/header.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { cn } from "@/lib/utils";
+import GitHubWhite from "@/public/github-mark-white.png";
+import GitHubBlack from "@/public/github-mark.png";
+import { SessionProvider } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { type FC } from "react";
+import { ModeToggle } from "../theme-toggle/theme-toggle";
+import { Breadcrumbs } from "./breadcrumbs";
+
+export const Header: FC = () => {
+	const pathname = usePathname();
+	const isOnTop = pathname === "/";
+
+	return (
+		<SessionProvider>
+			<header
+				className={cn(
+					"flex h-16 items-center justify-between sticky top-0 z-10 bg-background shadow-md dark:shadow-current",
+					isOnTop && "hidden",
+				)}
+			>
+				<div className="flex items-center gap-2 px-4">
+					<Breadcrumbs />
+				</div>
+				<div className="flex items-center gap-2 lg:gap-4 px-4">
+					<Link
+						href="https://github.com/namidapoo/dev-recap"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<Image
+							src={GitHubWhite}
+							alt="GitHub"
+							width={20}
+							height={20}
+							className="hidden dark:block"
+						/>
+						<Image
+							src={GitHubBlack}
+							alt="GitHub"
+							width={20}
+							height={20}
+							className="dark:hidden"
+						/>
+					</Link>
+					<ModeToggle />
+				</div>
+			</header>
+		</SessionProvider>
+	);
+};

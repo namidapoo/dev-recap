186bf5a3fab832c06d3d019fd359502150d9ff2a
namidapoo <83203852+ve1997@users.noreply.github.com>
2024-12-31 21:55:14 +0900

    feat: ヘッダーに認証コンポーネントを追加

---
+++ app/recap/components/user-button.tsx
@@ -0,0 +1,54 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useSession } from "next-auth/react";
+import type { FC } from "react";
+import { SignIn, SignOut } from "./auth-components";
+
+export const UserButton: FC = () => {
+	const { data: session } = useSession();
+	if (!session?.user) return null;
+
+	return (
+		<div className="flex items-center gap-2">
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Button
+						variant="ghost"
+						className="relative h-8 w-8 border rounded-full"
+					>
+						<Avatar className="h-8 w-8">
+							{session.user.image && (
+								<AvatarImage
+									src={session.user.image}
+									alt={session.user.login}
+								/>
+							)}
+						</Avatar>
+					</Button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent className="w-56" align="end" forceMount>
+					<DropdownMenuLabel className="font-normal">
+						<div className="flex flex-col space-y-1">
+							<p className="font-medium text-sm leading-none">
+								{session.user.login}
+							</p>
+							<p className="text-muted-foreground text-xs leading-none">
+								{session.user.email}
+							</p>
+						</div>
+					</DropdownMenuLabel>
+					<DropdownMenuItem>
+						<SignOut />
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</div>
+	);
+};

3f047f8be57fb7b28ee319a8a51fbf6b18468635
namidapoo <83203852+ve1997@users.noreply.github.com>
2024-12-31 21:57:30 +0900

    feat: `/recap`にアクセスされた際の404ページ追加

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
+
 export const runtime = "edge";
+
 export default function NotFound() {
 	return (
-		<div className="flex min-h-screen flex-col items-center justify-center bg-background text-foreground">
+		<div className="flex h-full flex-col items-center justify-center bg-background text-foreground">
 			<div className="space-y-4 text-center">
 				<h1 className="font-extrabold text-4xl tracking-tight lg:text-5xl">
 					404
